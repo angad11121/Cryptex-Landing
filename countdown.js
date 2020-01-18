5046e992eb58a1f6429af998b644f45018a8e5a8
Rachit Keerti Das <rachitkdas@gmail.com>
2020-01-18 16:20:02 +0530

    Countdown.js changed

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 24, 2020 10:00:00").getTime();
+var countDownDate = new Date("Jan 24, 2020 18:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -17,12 +17,13 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("LandingDiveIN").innerHTML = days + " D " + hours + " H "
-  + minutes + " M " + seconds + " S ";
+  document.getElementById("countdown").innerHTML = days + " : " + hours + " : "
+  + minutes + " : " + seconds;
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("LandingDiveIN").innerHTML = "DIVE IN!";
+    document.getElementById("countdown").innerHTML = "DIVE IN!";
   }
 }, 1000);
+
